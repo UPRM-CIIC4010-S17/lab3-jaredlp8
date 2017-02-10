eed52e8843a35147d9acedbd0bbf9aa8801b39ad
hjalmar <hjalmar@Jared>
2017-02-09 20:44:35 -0400

    Added lines and color to the window

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Jared Lopez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 450);
-		myFrame.setSize(200,200);
+		myFrame.setSize(300,300);
+		MyPanelClass myPanel = new MyPanelClass(); 
+		 myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
